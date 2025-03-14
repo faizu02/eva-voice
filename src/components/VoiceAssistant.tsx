d40622c4c2a2ab907d7430b03573f05d0a241ad8
faizu02 <faizalbashasfaizalbashas@gmail.com>
2025-03-14 09:09:47 +0530

    Update VoiceAssistant.tsx

--- src/components/VoiceAssistant.tsx
+++ src/components/VoiceAssistant.tsx
@@ -317,7 +317,7 @@ export const VoiceAssistant = () => {
     </div>
   )}
 
-  <div className=" bg-gradient-to-t from-background to-transparent">
+  <div className=" bg-gradient-to-t from-background to-transparent p-0 m-0">
    <div className="fixed bottom-0 left-100 right-100 p-4 pt-20">
     <div className="text-center  mt-40">
       <div className="relative">
